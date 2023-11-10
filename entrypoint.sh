3721ff410beab5198183d320dbd0b9fd4c15e975
HUEZO <huezo@users.noreply.github.com>
2023-11-09 18:24:42 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-
-# sleep 2m
-sleep 5m
-
 # Run Behave tests
 behave
 
 pwd
 
+# Copy the generated results to the shared volume
+cp -r /app/allure-results/* /allure-results
+
 # Run the post execution python script which uploades the pdf report to Confluence
 python3 /app/upload_report.py
 
-# Copy the generated results to the shared volume
-cp -r /app/allure-results/* /allure-results
+# sleep 2m
+#sleep 5m
 
