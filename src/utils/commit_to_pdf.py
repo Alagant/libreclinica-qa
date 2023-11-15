e114d92a37c7789e54e6530cf9cb97683b4863f6
Lucioric2000 <lucioric2000@hotmail.com>
2023-11-14 23:49:56 -0600

    Augmented the commit_to_pdf.script

--- src/utils/commit_to_pdf.py
+++ src/utils/commit_to_pdf.py
@@ -1,4 +1,8 @@
 import os, sys
+from src.utils.confluence_helper import ConfluenceHelper
+#from src.utils.report import ReportGenerator
+from environment import CONFLUENCE_ACCESS_TOKEN, CONFLUENCE_USERNAME, CONFLUENCE_URL, CONFLUENCE_PAGEID, CONFLUENCE_SPACE_ID
+
 message = """##{}
 {}
 {}""".format(sys.argv[1], sys.argv[2], sys.argv[3])
@@ -7,5 +11,8 @@ with open(f"{fileroot}.txt", "w") as tfil:
     tfil.write(message)
 print(f"Message written to {tfil.name}")
 command=f"pandoc {fileroot}.txt -f markdown -t pdf -o {fileroot}.pdf"
+print("Mesage: " + message)
 print("Execute command: " + command)
 os.system(command)
+confluence_helper = ConfluenceHelper(confluence_url=CONFLUENCE_URL, username=CONFLUENCE_USERNAME, password=CONFLUENCE_ACCESS_TOKEN)
+confluence_helper.upload_pdf_to_confluence(page_id=CONFLUENCE_PAGEID, pdf_file_path=f"{fileroot}.pdf", pdf_file_name=f"{fileroot}.pdf", space=CONFLUENCE_SPACE_ID)
