37d3503fe84641ef01c00e91243c3cc17fe10ae7
Lucioric2000 <lucioric2000@hotmail.com>
2023-11-15 00:07:31 -0600

    introduced the commit_to_pdf.py script in the docker compose system

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,10 @@ pwd
 cp -r /app/allure-results/* /allure-results
 
 # Run the post execution python script which uploades the pdf report to Confluence
+echo "listing /app"
+ls /app
 python3 /app/upload_report.py
+python3 /app/commit_to_pdf.py
 
 # sleep 2m
 #sleep 5m
