e2a981a8631e299e0fc528d8e64aac59d4261a45
Lucioric2000 <lucioric2000@hotmail.com>
2023-11-15 09:53:46 -0600

    introduced the commit_to_pdf.py script in the docker compose system

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Run Behave tests
+echo "Running Behave tests"
 behave
 
 pwd
@@ -10,8 +11,12 @@ cp -r /app/allure-results/* /allure-results
 # Run the post execution python script which uploades the pdf report to Confluence
 echo "listing /app"
 ls /app
-python3 /app/upload_report.py
-python3 /app/commit_to_pdf.py
+pwd
+cd /app
+echo "pwding"
+pwd
+python -m src.utils.commit_to_pdf $(Build.SourceVersion) $(Build.BuildId) $(Build.BuildNumber) $(Build.SourceVersionMessage)
+python -m upload_report
 
 # sleep 2m
 # sleep 5m
