32d32de6f065d24ec943ae7da0fb4b1834baecfe
Lucioric2000 <lucioric2000@hotmail.com>
2023-11-15 22:17:37 -0600

    introduced the commit_to_pdf.py script in the docker compose system

--- src/utils/commit_to_pdf.py
+++ src/utils/commit_to_pdf.py
@@ -1,4 +1,4 @@
-import os, sys
+import os, sys, tempfile
 from src.utils.confluence_helper import ConfluenceHelper
 from environment import CONFLUENCE_ACCESS_TOKEN, CONFLUENCE_USERNAME, CONFLUENCE_URL, CONFLUENCE_PAGEID, CONFLUENCE_SPACE_ID
 
@@ -8,13 +8,13 @@ message = """##Commit {}\n
 {}\n
 {}""".format(os.environ['source_version'], os.environ['build_id'], os.environ['build_number'], os.environ['commit_message'])
 fileroot = "message"
-with open(f"{fileroot}.txt", "w") as tfil:
-    tfil.write(message)
-print(f"Message written to {tfil.name}")
-command=f"pandoc {fileroot}.txt -f markdown -t pdf -o {fileroot}.pdf"
+tfile = tempfile.NamedTemporaryFile(mode="w", delete=False)
+tfile.write(message)
+print(f"Message written to {tfile.name}")
+command=f"pandoc {tfile.name} -f markdown -t pdf -o {tfile.name}.pdf"
 print("Mesage: " + message)
 print("Execute command: " + command)
 os.system(command)
 print(f"Uploading to Confluence URL {CONFLUENCE_URL}, with username {CONFLUENCE_USERNAME} and pageid {CONFLUENCE_PAGEID}")
 confluence_helper = ConfluenceHelper(confluence_url=CONFLUENCE_URL, username=CONFLUENCE_USERNAME, password=CONFLUENCE_ACCESS_TOKEN)
-confluence_helper.upload_pdf_to_confluence(page_id=CONFLUENCE_PAGEID, pdf_file_path=f"{fileroot}.pdf", pdf_file_name=f"{fileroot}.pdf", space=CONFLUENCE_SPACE_ID)
+confluence_helper.upload_pdf_to_confluence(page_id=CONFLUENCE_PAGEID, pdf_file_path=f"{fileroot}.pdf", pdf_file_name="message.pdf", space=CONFLUENCE_SPACE_ID)
